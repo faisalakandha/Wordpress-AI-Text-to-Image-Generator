fd075cb3d77cd58e6b73d7c078af76e9a73c3938
faisalakandha <57830958+faisalakandha@users.noreply.github.com>
2022-11-14 12:52:20 +0600

    Initial Plugin

---
+++ aiimggenerator.php
@@ -0,0 +1,11 @@
+<?php
+/**
+* Plugin Name: AiImgGenerator
+* Plugin URI: https://mywebsite.com/
+* Description: A plugin for AI Image Generation
+* Version: 1.0.0
+* Author: H.A.B.M. Faisal Akandha
+* Author URI: https://faisal.github.io/
+* License: GPL2
+**/
+?>
